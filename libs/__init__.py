a94b73908208080ae9a7e186f348ded63d9b03b4
SuperSonic <supersonic@livemail.tw>
2019-12-31 21:45:14 +0800

    Add License Notice

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Yuuki_Libs
+(c) 2019 Star Inc.
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
 from .connection import Yuuki_Connection
 from .yuuki import Yuuki, Yuuki_Settings
 
--- libs/yuuki.py
+++ libs/yuuki.py
@@ -1,6 +1,11 @@
-#!/usr/bin/python3
-# coding=UTF-8
-
+# -*- coding: utf-8 -*-
+"""
+Yuuki_Libs
+(c) 2019 Star Inc.
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
 import json
 import ntpath
 import os
